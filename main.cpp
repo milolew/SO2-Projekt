9dc4d3175a74fa2b8c08e8d9d7c8071767f77bb8
Miłosz <milolewtv@gmail.com>
2025-03-11 14:01:58 +0100

    Klasa DinningTable
    Wprowadzenie klasy odpowiedzialnej za logikę stołu. Wprowadzenie mechaniki jedzenia i czekania.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ private:
 
 public:
     Semaphore() : flag(false) {}
+
     void acquire()
     {
         bool expected = false;
@@ -21,9 +22,16 @@ public:
             std::this_thread::yield();
         }
     }
+
     void release()
     {
         flag.store(false);
+    }
+
+    bool try_acquire()
+    {
+        bool expected = false;
+        return flag.compare_exchange_weak(expected, true);
     }
 };
 
@@ -50,23 +58,101 @@ public:
     }
 };
 
-// Main philosopher method
-// TODO: Expand with actual logic for picking up and putting down forks
-void philosopher_function(int id, int total_philosophers, ConsoleLock &console)
+// TODO: Expand with deadlock prevention mechanism
+class DiningTable
 {
-    for (int i = 0; i < 5; i++)
+private:
+    int num_philosophers;
+    std::vector<Semaphore> forks;
+    std::vector<PhilosopherState> states;
+    ConsoleLock console;
+
+public:
+    DiningTable(int n) : num_philosophers(n), forks(n), states(n, THINKING) {}
+
+    // Attempt to pick up forks
+    bool try_pickup_forks(int id)
+    {
+        int left_fork = id;
+        int right_fork = (id + 1) % num_philosophers;
+
+        // Try to pick up left fork
+        if (!forks[left_fork].try_acquire())
+        {
+            return false;
+        }
+
+        // Try to pick up right fork
+        if (!forks[right_fork].try_acquire())
+        {
+            // Failed, put down left fork
+            forks[left_fork].release();
+            return false;
+        }
+
+        return true;
+    }
+
+    // Put down forks
+    void put_down_forks(int id)
+    {
+        int left_fork = id;
+        int right_fork = (id + 1) % num_philosophers;
+
+        forks[left_fork].release();
+        forks[right_fork].release();
+    }
+
+    void set_state(int id, PhilosopherState new_state)
+    {
+        states[id] = new_state;
+    }
+
+    PhilosopherState get_state(int id) const
+    {
+        return states[id];
+    }
+
+    void log(const std::string &message)
+    {
+        console.print(message);
+    }
+};
+
+// TODO: Implement simulation control mechanism and deadlock handling
+void philosopher_function(int id, DiningTable &table)
+{
+    const int cycles = 10; // Number of thinking/eating cycles
+
+    for (int i = 0; i < cycles; i++)
     {
         // Thinking
-        console.print("Philosopher " + std::to_string(id) + " is thinking");
+        table.set_state(id, THINKING);
+        table.log("Philosopher " + std::to_string(id) + " is thinking");
         std::this_thread::sleep_for(std::chrono::milliseconds(500 + rand() % 500));
 
-        // Trying to eat
-        console.print("Philosopher " + std::to_string(id) + " would like to eat");
+        // Trying to pick up forks
+        table.set_state(id, HUNGRY);
+        table.log("Philosopher " + std::to_string(id) + " is hungry and trying to pick up forks");
+
+        // Keep trying until both forks are picked up
+        while (!table.try_pickup_forks(id))
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+        // Eating
+        table.set_state(id, EATING);
+        table.log("Philosopher " + std::to_string(id) + " is eating");
         std::this_thread::sleep_for(std::chrono::milliseconds(500 + rand() % 500));
+
+        // Putting down forks
+        table.put_down_forks(id);
+        table.log("Philosopher " + std::to_string(id) + " puts down forks");
     }
 }
 
-// TODO: Implement a class to manage the table state and forks
+// TODO: Add simulation time limit and safe stopping mechanism
 int main(int argc, char *argv[])
 {
     if (argc != 2)
@@ -90,16 +176,16 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    ConsoleLock console;
+    DiningTable table(num_philosophers);
     std::vector<std::thread> threads;
 
-    // Creating threads for philosophers
+    // Create threads for philosophers
     for (int i = 0; i < num_philosophers; i++)
     {
-        threads.push_back(std::thread(philosopher_function, i, num_philosophers, std::ref(console)));
+        threads.push_back(std::thread(philosopher_function, i, std::ref(table)));
     }
 
-    // Waiting for all threads to complete
+    // Wait for all threads to finish
     for (auto &t : threads)
     {
         if (t.joinable())
